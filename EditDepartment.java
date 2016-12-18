a3bb181e3c0e9b330b98d3a38dc11ef60c0bde65
nszmn <eneszmn@hotmail.com>
2016-12-18 18:28:05 +0100

    Update EditDepartment.java

--- EditDepartment.java
+++ EditDepartment.java
@@ -1,24 +1,48 @@
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.FlowLayout;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumnModel;
+
+import com.mysql.jdbc.Connection;
+
+import FunctionImplementations.DBConnection;
+import Models.Department;
+
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
+
 import java.awt.Component;
+import java.awt.Dimension;
+import javax.swing.ScrollPaneConstants;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class EditDepartment extends JFrame {
 
 	private static JPanel contentPane;
 	private JTextField txtDepartmentName;
-	private static JTable tableDepartments;
-	private static JButton btnDelete;
+	private Connection DBConnection;
+	static Connection conn = new DBConnection().connect();
+	static PreparedStatement pstmt = null;
+	static ResultSet rs = null;
 
-	
+	/**
+	 * Launch the application.
+	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -32,7 +56,12 @@ public class EditDepartment extends JFrame {
 		});
 	}
 
-	public EditDepartment() { 
+	/**
+	 * Create the frame.
+	 * 
+	 * @throws SQLException
+	 */
+	public EditDepartment() throws SQLException {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -50,10 +79,26 @@ public class EditDepartment extends JFrame {
 		contentPane.add(txtDepartmentName);
 
 		JButton btnEdit = new JButton("Edit");
+		btnEdit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Department department = new Department();
+				department.setName(txtDepartmentName.getText());
+				if (department.getName().isEmpty()) {
+					JOptionPane.showMessageDialog(null, "Input cannot be empty!");
+					return;
+				}
+				String sql = "UPDATE Departments set Name = ? WHERE Name = ?";
+				try {
+					pstmt = conn.prepareStatement(sql);
+					pstmt.setString(1, department.getName());
+					pstmt.setString(2, department.getName());
+					int i = pstmt.executeUpdate();
+				} catch (SQLException e1) {
+					JOptionPane.showMessageDialog(null, "Entered department name already exists!");
+				}
+			}
+		});
 		btnEdit.setBounds(75, 127, 117, 29);
 		contentPane.add(btnEdit);
-		tableDepartments.setLocation(161, 34);
-
-		btnDelete = new JButton("Delete");
+	}
 }
-
